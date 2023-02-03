a03736fab0c34059cc830ed76583f000ff49154e
Joan <47475754+joanrodas@users.noreply.github.com>
2023-02-03 18:52:11 +0100

    Update Blade.php

--- src/Blade.php
+++ src/Blade.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Lexdubyna\Blade;
+namespace SirveliaLabs\Blade;
 
 use Illuminate\Contracts\Foundation\Application as ApplicationContract;
 use Illuminate\Contracts\View\Factory as FactoryContract;
